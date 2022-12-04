5e016fc19dc43dc4515d51edc77d7273142064b9
Erali <eralitemirov129@gmail.com>
2022-12-04 13:27:19 +0500

    qw

---
+++ app.js
@@ -0,0 +1,47 @@
+let pul = document.querySelector(".pul");
+
+let nonn = document.querySelector(".nonn");
+let input1 = document.querySelector("#non");
+input1.addEventListener("click", function () {
+  nonn.style.backgroundColor = "rgb(210, 93, 50)";
+  nonn.style.border = "5px solid rgb(104, 2, 2)";
+  pul.textContent++;
+  pul.textContent++;
+});
+let mushroom = document.querySelector(".mush");
+let input2 = document.querySelector("#qoziqorn");
+input2.addEventListener("click", function () {
+  mushroom.style.opacity = "1";
+  pul.textContent++;
+  pul.textContent++;
+  pul.textContent++;
+});
+
+let tomato = document.querySelector(".tomato");
+let input3 = document.querySelector("#pomidor");
+input3.addEventListener("click", function () {
+  tomato.style.opacity = "1";
+  pul.textContent++;
+});
+let zaytun = document.querySelector(".zay_tun");
+let input4 = document.querySelector("#zaytun");
+input4.addEventListener("click", function () {
+  zaytun.style.opacity = "1";
+  pul.textContent++;
+  pul.textContent++;
+});
+let bodring = document.querySelector(".bodrin");
+let input5 = document.querySelector("#bodring");
+input5.addEventListener("click", function () {
+  bodring.style.opacity = "1";
+  pul.textContent++;
+  pul.textContent++;
+});
+
+let kolbasa = document.querySelector(".kolbas");
+let input6 = document.querySelector("#kolbasa");
+input6.addEventListener("click", function () {
+  kolbasa.style.opacity = "1";
+  pul.textContent++;
+  pul.textContent++;
+});
